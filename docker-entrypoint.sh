b270695c7691a8fcdce2d39ce0802b2bf07ee005
Jeffre <jguymon@gmail.com>
2017-06-23 20:24:19 -0700

    Moved repeat-worthy scripting here instead of Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,40 @@ else
 fi
 
 
+# Change catalina.sh to 'run' (prevent deamonizing)
+# Remove `nohup` (prevent backgrounding)
+# Change CATALINA_PID path (obsr.pid) to a directory `ahsay` can read/write
+sed -i bin/startup.sh \
+    -e 's/catalina.sh" start.*/catalina.sh" run/g' \
+    -e 's/nohup//g' \
+    -e 's@/var/run/obsr\.pid@/obsr/obsr.pid@g'
+
+sed -i bin/shutdown.sh\
+    -e 's@/var/run/obsr\.pid@/obsr/obsr.pid@g'
+
+
+# Use ports 8080 and 8443
+# Replace depreciated 'sslProtocols' attribute AND enable SSLv2Hello
+# Add requested hostname ("local server name") to Tomcat access logs
+sed -i conf/server.xml \
+    -e 's/port="80"/port="8080"/g' \
+    -e 's/port="443"/port="8443"/g' \
+    -e 's/sslProtocols="TLSv1"/sslEnabledProtocols="TLSv1,SSLv2Hello"/g' \
+    -e "s/pattern=\"common\"/pattern='%h %l %u %t %v "%r" %s %b'/g"
+
+
+# Trust X-Forwarded-IPs from internal addresses
+if (! grep -q "org.apache.catalina.valves.RemoteIpValve" conf/server.xml); then
+  sed -i conf/server.xml \
+      -e 's@</Host>@  \
+        <Valve className="org.apache.catalina.valves.RemoteIpValve" \
+          remoteIpHeader="X-Forwarded-For" \
+          internalProxies="172.1[6-9]\.\d+\.\d+, 172.2[0-9]\.\d+\.\d+, \
+          172.3[0-1]\.\d+\.\d+, 10\.\d+\.\d+\.\d+" /> \
+      </Host>@g'
+fi
+
+
 # Stop catalina properly to avoid corrupting Profile.xml files and then exit
 #   the container with exitcode 143 (128 + 15)
 catalina_stop () {
